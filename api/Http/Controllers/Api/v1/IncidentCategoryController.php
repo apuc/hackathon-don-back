5bdb67aef841d43ce712e168d180a2756d292228
iIronside <iIronside@users.noreply.github.com>
2022-02-01 11:17:08 +0300

    refactoring IncidentCategory

--- api/Http/Controllers/Api/v1/IncidentCategoryController.php
+++ api/Http/Controllers/Api/v1/IncidentCategoryController.php
@@ -2,6 +2,8 @@
 
 namespace Api\Http\Controllers\Api\v1;
 
+use Api\Http\Resources\v1\IncidentCategoryResource;
+use Api\Services\Petition\IncidentCategoryService;
 use App\Repositories\IncidentCategory\IncidentCategoryRepository;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
@@ -9,27 +11,30 @@ use App\Http\Controllers\Controller;
 class IncidentCategoryController extends Controller
 {
     protected $incidentCategoryRepository;
+    protected $incidentCategoryService;
 
-    public function __construct(IncidentCategoryRepository $incidentCategory)
+    public function __construct(IncidentCategoryRepository $incidentCategory, IncidentCategoryService $incidentCategoryService)
     {
         $this->incidentCategoryRepository = $incidentCategory;
+        $this->incidentCategoryService = $incidentCategoryService;
     }
 
     public function show($category_id = null): JsonResponse
     {
-        if(!empty($category_id)) {
-            $categories = $this->incidentCategoryRepository->findById($category_id);
-        }
-        else {
-            $categories = $this->incidentCategoryRepository->getAll();
-        }
+        $categories = $this->incidentCategoryService->show($category_id);
 
         if (empty($categories)) {
-            return response()->json('User not found', 404);
+            return response()->json('Category not found', 404);
+        }
+
+        if($category_id != null) {
+            return response()->json(['success' => true,
+                IncidentCategoryResource::make($categories)
+            ]);
         }
 
         return response()->json(['success' => true,
-            'data' => $categories
+            IncidentCategoryResource::collection($categories)
         ]);
     }
 }
