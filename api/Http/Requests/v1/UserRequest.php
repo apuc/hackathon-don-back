13392d3b2e3c124ec6c6d18b3c602b5e0606a1a3
iIronside <iIronside@users.noreply.github.com>
2022-02-01 15:10:54 +0300

    add required phone

--- api/Http/Requests/v1/UserRequest.php
+++ api/Http/Requests/v1/UserRequest.php
@@ -25,10 +25,10 @@ class UserRequest extends FormRequest
     {
         return [
 
-            'name' => 'required|string',
+            'name' => 'string',
             'password' => 'required',
             'email' => 'email',
-            'phone' => 'numeric',
+            'phone' => 'required|numeric',
 
             'address' => 'array|required',
             'address.longitude' => 'required',
