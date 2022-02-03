147f2f494cbb99b10d79bcf68b6aad40add2cec1
iIronside <iIronside@users.noreply.github.com>
2022-02-03 10:43:07 +0300

    add ID to petition resource

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Main Page</title>
 </head>
 
@@ -21,59 +21,55 @@
     })();
 </script>
 <!--End of Tawk.to Script-->
+
 <body>
-    <div class="wrapper">
-        <header class="header">
-            <div class="container">
-                <div class="header-logo">
-                    <a href="#" class="header-logo__link">
-                        <img class="header-logo__img" src="../images/Logo.png" alt="Logo">
+<header class="header">
+    <div class="container">
+        <div class="header-logo">
+            <a href="#" class="header-logo__link">
+                <img class="header-logo__img" src="./images/Logo.png" alt="Logo">
+            </a>
+        </div>
+    </div>
+</header>
+<section class="main-page">
+    <div class="container">
+        <div class="main-page__main-box">
+            <!-- 1 -->
+            <div class="main-page__box-one">
+                <div class="dpr-card">
+                    <div class="dpr-card__title-text">Хочешь держать руку
+                        на пульсе событий?</div>
+                    <div class="dpr-card__box-links">
+                        <a href="#" class="dpr-card__links"><img class="dpr-card__google-play" src="./images/Google-play.png" alt="Google-play"></a>
+                        <a href="#" class="dpr-card__links"><img class="dpr-card__app-store" src="./images/App-store.png" alt="App-store"></a>
+                    </div>
+                </div>
+            </div>
+            <!-- 2 -->
+            <div class="card-form__container">
+                <h1 class="card-form__title-text">Первое приложение в ДНР <span class="card-form__title-span">для народа!</span></h1>
+                <ul class="card-form__list">
+                    <li class="card-form__item">без цензуры </li>
+                    <li class="card-form__item">только честные новости</li>
+                    <li class="card-form__item">размещайте свою новость <a href="#" class="card-form__link">прямо сейчас</a></li>
+                </ul>
+                <div class="card-form__card">
+                    <div class="card-form__subject-text">Нашли проблему, сообщи всем!</div>
+                    <form action="#" class="card-form__form">
+                        <input placeholder="Введите ваше имя" type="text" class="card-form__input">
+                        <input placeholder="Ваш номер телефона" type="tel" class="card-form__input">
+                    </form>
+                    <a href="#" class="card-form__btn-link">
+                        <div class="card-form__btn-bg">Отправить заявку</div>
                     </a>
                 </div>
             </div>
-        </header>
-        <section class="main-title">
-            <div class="container">
-                <div class="title-bg"></div>
-                <div class="main-title__box">
-                    <!-- 1 -->
-                    <div class="card-store__container">
-                        <div class="card-store__box">
-                            <div class="card-store__title-text">Хочешь держать руку
-                                на пульсе событий?
-                                <div class="card-store__store-links">
-                                    <a href="#" class="card-store__link-img">
-                                        <img src="../images/Google-play.png" alt="Google-play">
-                                    </a>
-                                    <a href="#" class="card-store__link-img">
-                                        <img src="../images/App-store.png" alt="App-store">
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- 2 -->
-                    <div class="card-form__container">
-                        <h1 class="card-form__title-text">Первое приложение в ДНР <span class="card-form__title-span">для народа!</span></h1>
-                        <ul class="card-form__list">
-                            <li class="card-form__item">без цензуры </li>
-                            <li class="card-form__item">только честные новости</li>
-                            <li class="card-form__item">размещайте свою новость <a href="#" class="card-form__link">прямо сейчас</a></li>
-                        </ul>
-                        <div class="card-form__card">
-                            <div class="card-form__subject-text">Нашли проблему, сообщи всем!</div>
-                            <form action="#" class="card-form__form">
-                                <input placeholder="Введите ваше имя" type="text" class="card-form__input">
-                                <input placeholder="Ваш номер телефона" type="tel" class="card-form__input">
-                            </form>
-                            <a href="#" class="card-form__btn-link">
-                                <div class="card-form__btn-bg">Отправить заявку</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        </div>
     </div>
+</section>
+</div>
+</div>
+</section>
 </body>
 </html>
